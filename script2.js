dfa89bae18e54be9654b1ffa071f457dde9c1bb1
V <vivek.agrawal@imaginea.com>
2017-01-08 15:41:36 +0530

    added 10th line b3

--- script2.js
+++ script2.js
@@ -5,3 +5,6 @@ Fourth line in branch 4
 Fifth line added in branch4 after checking out old commit
 Sixth line added in branch 3
 Sevent line added in branch 3
+Eigth line added in branch 3
+Nineth line added in branch 3
+10th branch 3
