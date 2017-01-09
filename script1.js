b0cf02946300eff577ff78a43b1ce4fd20814c93
netvgit <netvgit@users.noreply.github.com>
2017-01-09 15:24:26 +0530

    8th Line in branch 2 online

--- script1.js
+++ script1.js
@@ -5,3 +5,4 @@ Fourth line added  in b2
 Fifth line added in b2
 Sixth line added in local branch to be checked with remote for push
 Seventh line added in branch 2 online
+8th Line in branch 2 online
